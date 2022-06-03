c799d97f31cf152970846485f195833bc8543e3d
Aryaman Das <67958786+ARYAMAN-2001@users.noreply.github.com>
2022-06-03 14:52:36 +0530

    Committing files

---
+++ CodeChef/C++14/CONSADD/43561390.cpp
@@ -0,0 +1,111 @@
+using namespace std;//****************************Template Begins****************************//
+ 
+// Header Files
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<list> 
+#include<iterator>
+#include<deque>  
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<map>
+#include<unordered_map>
+#include<stdio.h>
+#include<complex>
+#include<math.h>
+#include<cstring>
+#include<string>
+// Header Files End  
+ 
+using namespace std;
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define umap unordered_map
+#define uset unordered_set
+#define lb lower_bound
+#define ub upper_bound
+#define fo(i,a,b) for(i=a;i<b;i++)
+#define all(v) (v).begin(),(v).end()
+#define all1(v) (v).begin()+1,(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define allr1(v) (v).rbegin()+1,(v).rend()
+#define sort0(v) sort(all(v))
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),max((c),(d)))
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define inf 9999999999999
+const ll mod=1e9+7;
+ 
+ll inv(ll i){if(i==1) return 1;return (mod-((mod/i)*inv(mod%i))%mod)%mod;}
+ 
+ll gcd(ll a,ll b){ if (b==0) return a;return gcd(b,a%b);}
+ 
+ll pwr(ll a, ll b) {a %= mod; ll res = 1;while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;}return res;}
+//****************************Template Ends*******************************//
+ 
+
+int main() {
+    fio;
+	long long t;
+	cin>>t;
+	while(t--){
+	    ll r,c,k,i,j,x,res=0;
+	    cin>>r>>c>>k;
+	    ll a[r][c],b[r][c];
+	    fo(i,0,r)
+	    {
+	        fo(j,0,c)
+	        cin>>a[i][j];
+	    }
+	    fo(i,0,r)
+	    {
+	        fo(j,0,c)
+	        cin>>b[i][j];
+	    }
+	    
+	    fo(i,0,r)
+	    {
+	        fo(j,1,c-(k-2))
+	        {
+	            fo(x,0,k-1)
+	            a[i][j+x]+=b[i][j-1]-a[i][j-1];
+	            
+	        }
+	    }
+	    fo(i,1,r-(k-2))
+	    {   
+	        fo(x,0,k-1)
+	        {
+	            fo(j,0,k-1)
+	            a[i+j][c-1-x]+=b[i-1][c-1-x]-a[i-1][c-1-x];
+	        }
+	    }
+	    fo(i,0,k-1)
+	    {
+	        fo(j,0,k-1)
+	        if(a[r-1-i][c-j-1]!=b[r-i-1][c-j-1])
+	        {res=1;
+	        break;}
+	    }
+	    if(res==0)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+    }
+    
+return 0;
+}

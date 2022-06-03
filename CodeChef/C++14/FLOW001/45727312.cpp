8bfc59f8e6aa5419db475dc6bbdfb69b07ccf38f
Aryaman Das <67958786+ARYAMAN-2001@users.noreply.github.com>
2022-06-03 14:54:18 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW001/45727312.cpp
@@ -0,0 +1,17 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include<iostream> 
+using namespace std;
+
+int main() {
+    int t,a,b,z;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {cin>>a>>b;
+    z=a+b;
+    cout<<z<<endl;
+
+	}
+
+	return 0;
+}

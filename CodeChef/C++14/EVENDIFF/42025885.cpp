fce549e646a794484ea5f4393371212026569df2
Aryaman Das <67958786+ARYAMAN-2001@users.noreply.github.com>
2022-06-03 14:52:53 +0530

    Committing files

---
+++ CodeChef/C++14/EVENDIFF/42025885.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include<vector>
+
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+#define fo(i,a,b) for(i=a;i<b;i++)
+#define all(v) (v).begin(),(v).end()
+#define all1(v) (v).begin()+1,(v).end()
+#define sort0(v) sort(all(v))
+
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),max((c),(d)))
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define inf 9999999999999
+const ll mod=1e9+7;
+ 
+ll inv(ll i){if(i==1) return 1;return (mod-((mod/i)*inv(mod%i))%mod)%mod;}
+ 
+ll gcd(ll a,ll b){ if (b==0) return a;return gcd(b,a%b);}
+ 
+ll pwr(ll a, ll b) {a %= mod; ll res = 1;while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;}return res;}
+using namespace std;
+
+int main() {
+    fio;
+	long long t;
+	cin>>t;
+	while(t--){
+         ll n,i;
+         cin>>n;
+         ll a[n],c=0;
+         fo(i,0,n)
+         {cin>>a[i];
+         if(a[i]%2==0)
+         c++;}
+         cout<<min(c,n-c)<<endl;
+	}
+	
+	return 0;
+}
